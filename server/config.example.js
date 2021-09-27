4b4032c237aa6306de13c7b23ef5ed844e9b0add
Suharsh Nagasampagi <suharsh329@gmail.com>
2021-09-27 21:33:17 +1000

    Initial commit

---
+++ server/config.example.js
@@ -0,0 +1,20 @@
+exports.db = {
+  "host": "HOSTNAME",
+  "database": "DATABASE",
+  "port": 5432,
+  "username": "YOUR_USERNAME",
+  "password": "YOUR_PASSWORD",
+  "reconnect": true,
+  "data_source_provider": "PROVIDER",
+  "type": "pg"
+}
+
+exports.secret = {
+  "SECRET": "YOUR_SECRET_KEY"
+}
+
+exports.aws = {
+  "ID": "ACCESS_ID",
+  "SECRET": "ACCESS_SECRET_KEY",
+  "BUCKET": "BUCKET_NAME"
+}
